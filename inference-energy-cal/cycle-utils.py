aacc9cc62ffa6bffd00b48d74813b100d7c38db7
Ruokai Yin <ryin25@wisc.edu>
2023-09-06 00:26:57 -0400

    Update new version of SATA_Sim

---
+++ inference-energy-cal/cycle-utils.py
@@ -0,0 +1,141 @@
+import yaml
+import os
+import shutil
+import subprocess
+import csv
+
+# Function Definitions
+
+def extract_scalesim_dict_from_yaml(filename):
+    with open(filename, 'r') as file:
+        data = yaml.safe_load(file)
+
+    scalesim_dict = {}
+
+    def recursive_search(subtrees):
+        for subtree in subtrees:
+            if subtree.get('class') == 'glbs':
+                locals_ = subtree.get('local', [])
+                # print(locals_)
+                for local in locals_:
+                    act_tag = local.get('act-tag')
+                    attributes = local.get('attributes', {})
+                    size = int(attributes.get('size-bytes')/1024)
+
+                    #! Adding support for multiple srams for one operand (e.g., 2 sram for ifmap)
+                    if "weight" in act_tag:
+                        scalesim_dict["FilterSramSzkB"] = size
+                    elif "ifmap" in act_tag:
+                        scalesim_dict["IfmapSramSzkB"] = size
+                    elif "ofmap" in act_tag:
+                        scalesim_dict["OfmapSramSzkB"] = size
+                    else:
+                        print(f"Warning: No matching sram class found for scalesim: {act_tag}")
+            elif subtree.get('class') == 'pe-array':
+                attributes = subtree.get('attributes', {})
+                scalesim_dict["ArrayWidth"] = attributes.get("width")
+                scalesim_dict["ArrayHeight"] = attributes.get("height")
+            else:
+                recursive_search(subtree.get('subtree', []))
+
+    recursive_search(data.get('architecture', {}).get('subtree', []))
+    if not "OfmapSramSzkB" in scalesim_dict.keys():
+        scalesim_dict["OfmapSramSzkB"] = scalesim_dict["IfmapSramSzkB"]
+
+    arch = data.get('architecture')
+    dataflow = arch.get('dataflow')
+    name = arch.get('name')
+    scalesim_dict['run_name'] = name
+    scalesim_dict['dataflow'] = dataflow
+
+    return scalesim_dict
+
+def update_config(yaml_filename, default_config_path, output_config_path):
+    # Load the YAML file
+    with open(yaml_filename, 'r') as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+
+    # Define a mapping from the placeholders to the YAML keys
+    mapping = {
+        "x0": "run_name",
+        "x1": "ArrayHeight",
+        "x2": "ArrayWidth",
+        "x3": "IfmapSramSzkB",
+        "x4": "FilterSramSzkB",
+        "x5": "OfmapSramSzkB",
+        "x6": "dataflow"
+    }
+
+    # Copy the original default.cfg to a new file
+    shutil.copy(default_config_path, output_config_path)
+
+    # Load the copied file
+    with open(output_config_path, 'r') as config_file:
+        config_content = config_file.read()
+
+    # Replace the placeholders with the values from the YAML file
+    for placeholder, key in mapping.items():
+        value = yaml_data.get(key, placeholder)  # Use the placeholder as default if key not found
+
+        config_content = config_content.replace(placeholder, str(value))
+
+    # Write the updated content back to the copied file
+    with open(output_config_path, 'w') as config_file:
+        config_file.write(config_content)
+
+    print(f"{output_config_path} updated successfully!")
+
+
+def generate_temp_workload():
+    with open('./workload.yaml', 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Convert YAML data to CSV rows
+    csv_rows = []
+    for layer in data.get('Layers', []):
+        attributes = layer.get('attributes', {})
+        row = [
+            layer.get('name', 'N/A'),
+            attributes.get('IFMAP Height', 'N/A'),
+            attributes.get('IFMAP Width', 'N/A'),
+            attributes.get('Filter Height', 'N/A'),
+            attributes.get('Filter Width', 'N/A'),
+            attributes.get('Channels', 'N/A'),
+            attributes.get('Num Filter', 'N/A'),
+            attributes.get('Strides', 'N/A'),
+            ''
+        ]
+        csv_rows.append(row)
+
+    # Write the CSV data to a file
+    csv_file_path = '../scale-sim-v2/temp_workload.csv'
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Layer name', 'IFMAP Height', 'IFMAP Width', 'Filter Height', 'Filter Width', 'Channels', 'Num Filter', 'Strides', ''])
+        csv_writer.writerows(csv_rows)
+
+    print("CSV file written successfully.")
+
+# Main Execution
+
+if __name__ == "__main__":
+    filename = 'sata-config.yaml'
+    scalesim_dict = extract_scalesim_dict_from_yaml(filename)
+    # print(scalesim_dict)
+    scalesim_path = '../scale-sim-v2'
+
+    output_filename = 'cycle-stat-temp.yaml'
+    with open(output_filename, 'w') as outfile:
+        yaml.dump(scalesim_dict, outfile, default_flow_style=False)
+    print(f"Cycle stats written to {output_filename}")
+
+
+    default_config_path = os.path.join('..', 'scale-sim-v2', 'configs', 'default.cfg')
+    output_config_path = os.path.join('..', 'scale-sim-v2', 'configs', 'running.cfg')
+
+    # Update the copied file with values from the YAML file
+    update_config(output_filename, default_config_path, output_config_path)
+    os.remove(output_filename)
+    generate_temp_workload()
+    os.chdir(scalesim_path)
+    subprocess.run('python3 run.py', shell=True)
---
+++ inference-energy-cal/energy-cal.py
@@ -0,0 +1,217 @@
+import yaml
+import os
+import math
+
+def read_cycles(cycle_filepath, arch_configpath, workload_filepath):
+
+    with open(arch_configpath, 'r') as file:
+        data = yaml.safe_load(file)
+    arch = data.get('architecture')
+    dataflow = arch.get('dataflow')
+    # timestep = arch.get('timestep')
+    with open(workload_filepath, 'r') as file:
+        work_data = yaml.safe_load(file)
+    general = work_data.get('General')
+    timestep = general.get('timestep')
+
+    with open(cycle_filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    
+    cycle_stats = {}
+    cycle_stats['total_cycles'] = 0.0
+    cycle_stats['sram_i_reads'] = 0.0
+    cycle_stats['sram_w_reads'] = 0.0
+    cycle_stats['sram_o_writes'] = 0.0
+    cycle_stats['dram_i_reads'] = 0.0
+    cycle_stats['dram_w_reads'] = 0.0
+    cycle_stats['dram_o_writes'] = 0.0
+
+    if dataflow == 'sata':
+        for layer, stats in data.items():
+            # print((stats.get('SRAM OFMAP Start Cycle') + stats.get('SRAM OFMAP Cycles'))*timestep)
+            cycle_stats['total_cycles'] += (stats.get('SRAM OFMAP Start Cycle') + stats.get('SRAM OFMAP Cycles'))*timestep + stats.get('SRAM Filter Cycles')
+            cycle_stats['sram_i_reads'] += stats.get('SRAM IFMAP Reads')
+            cycle_stats['sram_w_reads'] += stats.get('SRAM Filter Reads')
+            cycle_stats['sram_o_writes'] += stats.get('SRAM OFMAP Writes')
+            cycle_stats['dram_i_reads'] += stats.get('DRAM IFMAP Reads')
+            cycle_stats['dram_w_reads'] += stats.get('DRAM Filter Reads')
+            cycle_stats['dram_o_writes'] += stats.get('DRAM OFMAP Writes')
+    else:
+        for layer, stats in data.items():
+            cycle_stats['total_cycles'] += stats.get('SRAM OFMAP Start Cycle') + stats.get('SRAM OFMAP Cycles')
+            cycle_stats['sram_i_reads'] += stats.get('SRAM IFMAP Reads')
+            cycle_stats['sram_w_reads'] += stats.get('SRAM Filter Reads')
+            cycle_stats['sram_o_writes'] += stats.get('SRAM OFMAP Writes')
+            cycle_stats['dram_i_reads'] += stats.get('DRAM IFMAP Reads')
+            cycle_stats['dram_w_reads'] += stats.get('DRAM Filter Reads')
+            cycle_stats['dram_o_writes'] += stats.get('DRAM OFMAP Writes')
+    
+    return cycle_stats
+    
+
+
+def extract_workload(workload_filepath):
+    
+    with open(workload_filepath, 'r') as file:
+        work_data = yaml.safe_load(file)
+    general = work_data.get('General')
+    timestep = general.get('timestep')
+    sparsity = general.get('sparsity')
+
+    workload_dic = {}
+    layers = work_data.get('Layers', {})
+    # print(layers)
+    total_mac = 0.0
+    total_lif = 0.0
+
+    for l in layers:
+        # print(l)
+        attr = l['attributes']
+        of_w = (math.floor((attr['IFMAP Width'] + 2 - attr['Filter Width'])/attr['Strides']) + 1) # TODO: Adding support for different padding, now assume 1
+        of_h = (math.floor((attr['IFMAP Height'] + 2 - attr['Filter Height'])/attr['Strides']) + 1)
+        total_lif += attr['Num Filter'] * (of_w * of_h) * timestep
+        total_mac += attr['Num Filter'] * (of_w * of_h) * (attr['Filter Width'] * attr['Filter Height'] * attr['Channels']) * timestep * (1-sparsity)
+    
+    workload_dic['total_mac'] = int(total_mac)
+    workload_dic['total_lif'] = int(total_lif)
+    
+    return workload_dic
+
+
+    # print(sparsity)
+
+    # if dataflow == 'sata':
+def comp_computation_energy(comp_filepath, cycle_dict, arch_configpath, workload_dict):
+    
+    with open(arch_configpath, 'r') as file:
+        data = yaml.safe_load(file)
+    subtrees_ = data.get('architecture', {}).get('subtree', [])
+    for tree in subtrees_:
+        if tree.get('class') == 'pe-array':
+            attributes = tree.get('attributes')
+            height = attributes.get('height')
+            width  = attributes.get("width")
+    pe_size = height * width
+    arch = data.get('architecture')
+    freq = arch.get('clock-frequency') * 1000000 #! Need to convert to MHz first, in yaml, M is not take into consideration
+    cyc = 1/freq
+
+    with open(comp_filepath, 'r') as file:
+        comp_data = yaml.safe_load(file)
+
+    comp_dic = {}
+    total_comp = 0.0
+    for component, values in comp_data.items():
+        comp_dic[component] = {}
+        subtotal = 0.0
+        if 'lif' in component:
+            workload = workload_dict['total_lif']
+        else:
+            workload = workload_dict['total_mac']
+        # print(f"{component}:")
+        # ! Power of computation unit is in mW, from the comp-stat.yaml
+        convert_ratio = 1000000 # ! Need to convert it back to nJ, to align with the memory energy, which is in nJ
+        for key, value in values.items():
+            if isinstance(value, dict):
+                # print(key)
+                comp_dic[component]['energy-operation'] = value['y'] * workload * cyc * convert_ratio 
+                subtotal += comp_dic[component]['energy-operation']
+                comp_dic[component]['energy-ungated'] = value['n']*cycle_dict['total_cycles'] * cyc * convert_ratio * pe_size
+                subtotal += comp_dic[component]['energy-ungated']
+            elif 'lpower' in key:
+                # print(key)
+                comp_dic[component]['energy-leakage'] = value * cycle_dict['total_cycles'] * cyc * convert_ratio * pe_size
+                subtotal += comp_dic[component]['energy-leakage']
+        comp_dic[component]['total'] = subtotal
+        total_comp += subtotal
+    comp_dic['total'] = total_comp
+
+    file_path = './results/computation-energy.yaml'
+    # print()
+    with open(file_path, 'w') as yaml_file:
+        yaml.dump(comp_dic, yaml_file, default_flow_style=False)
+    return comp_dic
+
+
+def comp_mem_energy(mem_filepath, cycle_dict, arch_configpath):
+
+    with open(arch_configpath, 'r') as file:
+        arch_data = yaml.safe_load(file)
+    arch = arch_data.get('architecture')
+    freq = arch.get('clock-frequency') * 1000000 #! Need to convert to MHz first, in yaml, M is not take into consideration
+    cyc = 1/freq
+
+    convert_ratio = 1000000 #! Need again to convert SRAM leakage to nJ, the leaking power is in mW from the mem-stats.yaml
+    # timestep = arch.get('timestep')
+
+    with open(mem_filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    mem_dic = {}
+    dram_entries = data.get('DRAM', [])
+    total_dram = 0.0
+    for entry in dram_entries:
+        name = entry.get('name')
+        sub_total = 0.0
+        if name not in mem_dic:
+            mem_dic[name] = {}
+        mem_dic[name]['ifmap'] = entry.get('read energy') * cycle_dict['dram_i_reads'] # ? CHECK: is the energy in nJ ?
+        total_dram += mem_dic[name]['ifmap']
+        sub_total  += mem_dic[name]['ifmap']
+        mem_dic[name]['weight'] = entry.get('read energy') * cycle_dict['dram_w_reads'] # ? CHECK: is the energy in nJ ?
+        total_dram += mem_dic[name]['weight']
+        sub_total  += mem_dic[name]['weight']
+        mem_dic[name]['ofmap'] = entry.get('read energy') * cycle_dict['dram_o_writes'] # ? CHECK: is the energy in nJ ?
+        total_dram += mem_dic[name]['ofmap']
+        sub_total  += mem_dic[name]['ofmap']
+        mem_dic[name]['total'] = sub_total
+    mem_dic['dram_total'] = total_dram
+
+    sram_entries = data.get('SRAM', [])
+    total_sram = 0.0
+    for entry in sram_entries:
+        name = entry.get('name')
+        if name not in mem_dic:
+            mem_dic[name] = {}
+        if 'ifmap' in name:
+            mem_dic[name]['ifmap-dynamic'] = entry.get('read dynamic energy') * cycle_dict['sram_i_reads'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['ifmap-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio #! The power is in mW, need to scale it to nJ
+            mem_dic[name]['ifmap-total']= mem_dic[name]['ifmap-dynamic'] + mem_dic[name]['ifmap-leakage']
+            total_sram += mem_dic[name]['ifmap-dynamic'] + mem_dic[name]['ifmap-leakage']
+        elif 'weight' in name:
+            mem_dic[name]['weight-dynamic'] = entry.get('read dynamic energy') * cycle_dict['sram_w_reads'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['weight-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio
+            mem_dic[name]['weight-total']= mem_dic[name]['weight-dynamic'] + mem_dic[name]['weight-leakage']
+            total_sram += mem_dic[name]['weight-dynamic'] + mem_dic[name]['weight-leakage']
+        elif 'ofmap' in name:
+            mem_dic[name]['ofmap-dynamic'] = entry.get('write dynamic energy') * cycle_dict['sram_o_writes'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['ofmap-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio
+            mem_dic[name]['ofmap-total']= mem_dic[name]['ofmap-dynamic'] + mem_dic[name]['ofmap-leakage']
+            total_sram += mem_dic[name]['ofmap-dynamic'] + mem_dic[name]['ofmap-leakage']
+    mem_dic['sram_total'] = total_sram
+
+    # print(mem_dic)
+    file_path = './results/memory-energy.yaml'
+    # print()
+    with open(file_path, 'w') as yaml_file:
+        yaml.dump(mem_dic, yaml_file, default_flow_style=False)
+    return mem_dic
+
+
+if __name__ == "__main__":
+
+    cycle_path = './results/cycle-stat.yaml'
+    arch_path = './sata-config.yaml'
+    mem_path = './results/mem-stat.yaml'
+    comp_path = './results/comp-stat.yaml'
+    work_path = './workload.yaml'
+
+    cycle_stat = read_cycles(cycle_path, arch_path, work_path)
+    # print(cycle_stat)
+    
+    workload_dic = extract_workload(work_path)
+    # print(workload_dic)
+    mem_dic = comp_mem_energy(mem_path, cycle_stat, arch_path)
+    comp_dic = comp_computation_energy(comp_path, cycle_stat, arch_path, workload_dic)
+    
+    print("SATA_Sim simulaton successes. Please go to the result folder to locate the energy results.")
+
